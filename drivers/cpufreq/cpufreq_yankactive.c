d8cbb11d2c1c2bf78fe89641280b52a233283cd0
yank555-lu <yank555.lu@gmail.com>
2014-07-12 16:37:55 +0200

    cpufreq: Add yankdemand governor
    Full stock (JB) ondemand governor with changed default tunable values aimed at lower battery consumption
    
    Change-Id: I2006ae1ea67dd775aa1e2b50c316c9313452055b
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- drivers/cpufreq/cpufreq_yankactive.c
+++ drivers/cpufreq/cpufreq_yankactive.c
@@ -151,7 +151,7 @@ struct cpufreq_governor cpufreq_gov_yankactive = {
 	.owner = THIS_MODULE,
 };
 
-static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
+static inline cputime64_t uget_cpu_idle_time_jiffy(unsigned int cpu,
 						  cputime64_t *wall)
 {
 	u64 idle_time;
@@ -174,13 +174,13 @@ static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
 	return jiffies_to_usecs(idle_time);
 }
 
-static inline cputime64_t get_cpu_idle_time(unsigned int cpu,
+static inline cputime64_t uget_cpu_idle_time(unsigned int cpu,
 					    cputime64_t *wall)
 {
 	u64 idle_time = get_cpu_idle_time_us(cpu, wall);
 
 	if (idle_time == -1ULL)
-		idle_time = get_cpu_idle_time_jiffy(cpu, wall);
+		idle_time = uget_cpu_idle_time_jiffy(cpu, wall);
 	else if (!io_is_busy)
 		idle_time += get_cpu_iowait_time_us(cpu, wall);
 
@@ -195,7 +195,7 @@ static void cpufreq_yankactive_timer_resched(
 
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
-		get_cpu_idle_time(smp_processor_id(),
+		uget_cpu_idle_time(smp_processor_id(),
 				     &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
@@ -234,7 +234,7 @@ static void cpufreq_yankactive_timer_start(int cpu, int time_override)
 
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
-		get_cpu_idle_time(cpu, &pcpu->time_in_idle_timestamp);
+		uget_cpu_idle_time(cpu, &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
 	spin_unlock_irqrestore(&pcpu->load_lock, flags);
@@ -378,7 +378,7 @@ static u64 update_load(int cpu)
 	unsigned int cur_load = 0;
 #endif
 
-	now_idle = get_cpu_idle_time(cpu, &now);
+	now_idle = uget_cpu_idle_time(cpu, &now);
 	delta_idle = (unsigned int)(now_idle - pcpu->time_in_idle);
 	delta_time = (unsigned int)(now - pcpu->time_in_idle_timestamp);
 
--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -447,6 +447,9 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKDEMAND)
+extern struct cpufreq_governor cpufreq_gov_yankdemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankdemand)
 #endif
 
 
